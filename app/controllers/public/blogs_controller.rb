ed2eaf7b5725af69fc588208834eaa8a4a2b7910
NissyP <ishika.12.twelve@gmail.com>
2021-01-28 11:32:11 +0000

    1/28-4

--- app/controllers/public/blogs_controller.rb
+++ app/controllers/public/blogs_controller.rb
@@ -49,6 +49,9 @@ class Public::BlogsController < ApplicationController
   end
 
   def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path
   end
   
   private
